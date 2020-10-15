729a5069bc3621cd5fc3fad2b5071c904548f8f6
nerad25 <dmcrull@yahoo.com>
2020-10-15 16:01:52 -0500

    Added select and delete querys for user

--- Server/src/events.js
+++ Server/src/events.js
@@ -6,6 +6,7 @@ function createRouter(db) {
 
   // the routes are defined here
 
+  //INSERT USER
   router.post('/event', (req, res, next) => {
     db.query(
       'INSERT INTO user () VALUES (?)',
@@ -13,6 +14,39 @@ function createRouter(db) {
       (error) => {
         if (error) {
           console.error(error);
+          res.status(500).json({status: 'error'});
+        } else {
+          res.status(200).json({status: 'ok'});
+        }
+      }
+    );
+  });
+  // END INSERT USER
+
+  //SELECT FROM USER
+  router.get('/event', function (req, res, next) {
+    db.query(
+      'SELECT id FROM user WHERE userID=?',
+      [owner, 10*(req.params.page || 0)],
+      (error, results) => {
+        if (error) {
+          console.log(error);
+          res.status(500).json({status: 'error'});
+        } else {
+          res.status(200).json(results);
+        }
+      }
+    );
+  });
+  //END SELECT FROM USER
+
+  //DELETE USER
+  router.delete('/event/:id', function (req, res, next) {
+    db.query(
+      'DELETE FROM user WHERE userID=?',
+      [req.params.id, owner],
+      (error) => {
+        if (error) {
           res.status(500).json({status: 'error'});
         } else {
           res.status(200).json({status: 'ok'});
